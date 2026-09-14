de4f002af57e30b51818ce7fdb8ec7f057948250
Renato Lima <renatodelimaleoncio@gmail.com>
2026-09-14 00:29:09 -0300

    feat: versão inicial (app.py e README)

---
+++ EXERCÍCIOS/Operadores.py
@@ -0,0 +1,7 @@
+#ENTRADA DE DADOS
+nota1 = float(input("Digite a 1ª nota: "))
+nota2 = float(input("Digite a 2ª nota: "))
+#PROCESSAMENTO
+media = (nota1 + nota2) / 2
+#SAÍDA DE DADOS
+print(f"A média entre {nota1} e {nota2} é {media}")
--- primeiro-projeto/app.py
+++ primeiro-projeto/app.py
@@ -1,16 +1,16 @@
 # Programa de Cálculo de média de notas
-# Renato de Lima Leoncio
+# Autor: Renato de Lima Leoncio
 
-nome = (input("Digite o nome do aluno: "))
+# Entrada
+nome = input("Digite o nome do aluno: ")
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
-
+# Processamento
 media = (nota1 + nota2) / 2
-
+# Saída
 print(f"\nAluno: {nome}")
 print(f"Média: {media:.2f}")
 if media >= 6:
-    print("Situação: Aprovado")
+print("Situação: Aprovado")
 else:
-    print("Situação: Reprovado")
-    
+print("Situação: Reprovado")
